f69fbb3037f65a65b59814d92891af0e46a13c98
Max Cussans <max-cussans2@hotmail.co.uk>
2017-01-10 19:18:42 +0000

    WOW it works
    sort of black screen

--- Project/main.cpp
+++ Project/main.cpp
@@ -45,12 +45,12 @@ bool showHeatmap = false;
 //string holding the **source** of our vertex shader, to save loading from a file
 const std::string strVertexShader = R"(
 	#version 330
-	in vec4 position;
+	in vec3 position;
 	in vec4 vertexColor;
 	out vec4 fragmentColor;
 	void main()
 	{
-		 gl_Position = vec4(position, 0.0, 1.0);
+		 gl_Position = vec4(position, 1.0);
 		 fragmentColor = vertexColor;
 	}
 )";
